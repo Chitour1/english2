602dfea6c5634d8280a3d19688cb91ad290e6cc1
Chitour1 <chitour1@gmail.com>
2025-07-31 18:10:25 +0300

    Update a2_levels2.js

--- a2_levels2.js
+++ a2_levels2.js
@@ -1,7 +1,7 @@
 export const A2_SUB_GROUPS = [
   {
     groupName: "المستويات 1 - 10",
-    filePath: "https://cdn.jsdelivr.net/gh/Chitour1/english2/a2_levels_1-10.js" // رابط ملف المجموعة الأولى
+    filePath: "https://cdn.jsdelivr.net/gh/Chitour1/english2/a2_levels_01-10.js" // رابط ملف المجموعة الأولى
   },
   {
     groupName: "المستويات 11 - 20",

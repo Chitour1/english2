23edb1fae1bb7fd99da38bd1ce36395cbce940b4
Chitour1 <chitour1@gmail.com>
2025-07-31 16:57:32 +0300

    Create main_library.js

---
+++ main_library.js
@@ -0,0 +1,7 @@
+export const LIBRARY_INDEX = [
+  {
+    groupName: "المستوى A2",
+    filePath: "https://cdn.jsdelivr.net/gh/Chitour1/english2/a2_levels.js" // <-- تأكد من أن هذا هو الرابط الصحيح لملفك الجديد بعد رفعه
+  }
+  // هنا سنضيف روابط المستويات الجديدة مستقبلاً
+];

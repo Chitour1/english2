add9fe13ce3b26dda2bfc56b94a146499c271774
Chitour1 <chitour1@gmail.com>
2025-07-31 18:22:37 +0300

    Update and rename a2_levels2.js to a2_levels3.js

--- a2_levels3.js
+++ a2_levels3.js
@@ -5,7 +5,7 @@ export const A2_SUB_GROUPS = [
   },
   {
     groupName: "المستويات 11 - 20",
-    filePath: "https://.../a2_levels_11-20.js" // رابط ملف المجموعة الثانية
+    filePath: "https://cdn.jsdelivr.net/gh/Chitour1/english2/a2_levels_11-20.js" // رابط ملف المجموعة الثانية
   }
   // يمكنك إضافة مجموعة 21-30 هنا مستقبلاً بنفس الطريقة
 ];
